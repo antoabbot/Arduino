0e0f1d6fdae8264659c818cfd161241073877b89
antoabbot <ahabbot@gmail.com>
2015-12-27 23:03:38 +0000

    Initial Swift project

---
+++ BLE/BleTest/BleTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BleTest
+//
+//  Created by Anthony Abbot on 27/12/2015.
+//  Copyright © 2015 Anthony Abbot. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
